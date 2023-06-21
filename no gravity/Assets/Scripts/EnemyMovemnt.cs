0cd88e8bbc58d7a354615e55bc908c198b9d51b8
ilovetapwaterr <132835392+CNMarkham@users.noreply.github.com>
2023-06-21 17:49:47 -0400

    good step

---
+++ no gravity/Assets/Scripts/EnemyMovemnt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nemyMovemnt : MonoBehaviour
+{
+    public float yForce;
+    private Rigidbody2D enemyRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
