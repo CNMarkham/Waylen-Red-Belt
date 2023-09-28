9f3673f699401fb419e764724781c6f50ff43dc0
CNMarkham <markhamonca@codeninjas.com>
2023-09-28 17:55:02 -0400

    step 59

---
+++ Codeys gran turismo/Assets/Scripts/TriggerFinishLine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("You Win!");
+        }
+    }
+}
