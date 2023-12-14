d15336c2cdac00d5094736a13b7d93a34152c6b8
Cocomelon <markhamonca@codeninjas.com>
2023-12-14 17:51:36 -0500

    step 131

--- Codeys gran turismo/Assets/Scripts/itemBoxSpawner.cs
+++ Codeys gran turismo/Assets/Scripts/itemBoxSpawner.cs
@@ -7,10 +7,15 @@ public class itemBoxSpawner : MonoBehaviour
 
     public GameObject itemBox;
 
+    public int numberOfBoxes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < numberOfBoxes; i++)
+        {
+
+        }
     }
 
     // Update is called once per frame
