edd9f1ba5d4128a2f4e083b01d71d4e1048ce991
Cocomelon <markhamonca@codeninjas.com>
2023-12-28 17:52:14 -0500

    step 153

---
+++ Codeys gran turismo/Assets/Scripts/itemBoxFeatures.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxFeatures : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(69,69,69));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Codey")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void itemBoxRespawn()
+    {
+        //gameObject.SetActive(true);
+    }
+}
