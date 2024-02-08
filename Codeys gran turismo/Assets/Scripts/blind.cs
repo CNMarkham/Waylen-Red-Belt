f8b98cce19d39d13b4cbcc24da78a6eac31e79b4
Ryan <markhamonca@codeninjas.com>
2024-02-08 17:46:19 -0500

    step 182

---
+++ Codeys gran turismo/Assets/Scripts/blind.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blind : MonoBehaviour
+{
+    public Image image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        image=GameObject.Findcanvas/Image.getcomponent.Image
+        //make player blind
+        //make colour a 288
+        image.color = new Color(1, 1, 1, 1);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make colour a decrease by teeny tiny umpalumpa small amount
+        //detroy if colourrrrrrrrrrrrrrrrrrrrrrrrrrrr a reaches 00000000000
+    }
+}
